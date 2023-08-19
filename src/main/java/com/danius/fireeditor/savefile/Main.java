d09a53a4c055d81bdc89f6f5671f90387401c8c0
user1 <dani7mc8@gmail.com>
2023-08-19 22:02:01 +0200

    Fix the convoy item count on modded save files

--- src/main/java/com/danius/fireeditor/savefile/Main.java
+++ src/main/java/com/danius/fireeditor/savefile/Main.java
@@ -6,10 +6,6 @@ import com.danius.fireeditor.util.Hex;
 
 public class Main {
     public static void main(String[] args) {
-        String citraUs = "C:\\Users\\user1\\AppData\\Roaming\\Citra\\sdmc\\Nintendo 3DS\\00000000000000000000000000000000\\00000000000000000000000000000000\\title\\00040000\\000a0500\\data\\00000001\\";
-        String download = "C:\\Users\\user1\\Downloads\\Chapter0 (1)";
-        byte[] fileBytes = Hex.getFileBytes(download);
-        Hex.writeFile(SaveFile.decompressBytes(fileBytes, 0xC0), download);
 
     }
 
