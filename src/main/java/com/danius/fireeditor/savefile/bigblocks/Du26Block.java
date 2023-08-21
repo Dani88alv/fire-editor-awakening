92d75012af2e074e0bd747edf5e0231a1e281403
user1 <dani7mc8@gmail.com>
2023-08-21 20:52:14 +0200

    Fixing the SpotPass Unlocker

--- src/main/java/com/danius/fireeditor/savefile/bigblocks/Du26Block.java
+++ src/main/java/com/danius/fireeditor/savefile/bigblocks/Du26Block.java
@@ -7,17 +7,12 @@ import java.io.File;
 import java.util.Arrays;
 
 public class Du26Block {
-
-    private final byte[] rawStreet; //StreetPass Team and Einherjar Map Encounters (Ignored)
-    private byte[] rawSpot; //Downloadable Content
+    private final byte[] rawMain; //TODO: Divide the SpotPass/StreetPass encounters and StreetPass Team
     private final byte[] rawEnd; //Dlc Turns and EVST data
 
     public Du26Block(byte[] bytes) {
+        this.rawMain = Arrays.copyOfRange(bytes, 0x0, bytes.length - 0x98);
         this.rawEnd = Arrays.copyOfRange(bytes, bytes.length - 0x98, bytes.length);
-        this.rawSpot = Arrays.copyOfRange(bytes,
-                bytes.length - rawEnd.length - 0x20F, bytes.length - rawEnd.length);
-        this.rawStreet = Arrays.copyOfRange(bytes,
-                0x0, bytes.length - rawSpot.length - rawEnd.length);
     }
 
     public int dlcTurn(int slot) {
@@ -31,19 +26,21 @@ public class Du26Block {
     public void addSpotpass() {
         String relativePath = "com/danius/fireeditor/templates/blocks/rawSpotpass";
         File file = new File("src/main/resources/" + relativePath);
-        rawSpot = Hex.getFileBytes(String.valueOf(file));
+        byte[] rawSpot = Hex.getFileBytes(String.valueOf(file));
+        byte[] header = Hex.toByte("AD 55 0A 19 01");
+        int offset = Hex.indexOf(rawMain, header, 0x0, 0) - header.length;
+        System.arraycopy(rawSpot, 0, rawMain, offset, rawSpot.length);
     }
 
     public void setDuelRenown(int value) {
         int point = 0xE;
-        Hex.setByte4(rawStreet, point, value);
+        Hex.setByte4(rawMain, point, value);
     }
 
     public byte[] bytes() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
-            outputStream.write(rawStreet);
-            outputStream.write(rawSpot);
+            outputStream.write(rawMain);
             outputStream.write(rawEnd);
 
         } catch (Exception e) {
